654736bdf8c1db9980c119ca8473d395b85b79ae
m13303620317 <116149426+m13303620317@users.noreply.github.com>
2022-11-02 19:13:15 -0700

    Add files via upload

---
+++ Calculator.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace Day08App
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class Calculator : Page
+    {
+        public Calculator()
+        {
+            this.InitializeComponent();
+        }
+
+        // Add two numbers together
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = " + (a + b));
+        }
+
+        // Subtract two numbers
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = " + (a - b));
+        }
+
+        // Multiply two numbers
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} * {b} = " + (a * b));
+        }
+
+        // Divide two numbers by one
+        public static void Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("The divisor can not be zero!");
+            }
+            else
+            {
+                Console.WriteLine($"{a} / {b} = " + (a / b));
+            }
+        }
+
+        // Radication
+        public static void Radication(double a)
+        {
+            if (a < 0)
+            {
+                Console.WriteLine("The radicand can not less than 0!");
+            }
+            else
+            {
+                Console.WriteLine($"The square root of {a} is " + Math.Sqrt(a));
+            }
+        }
+
+        // Power
+        public static void Power(double a, double b)
+        {
+            Console.WriteLine($"{a} ^ {b} = " + Math.Pow(a, b));
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace Day08App
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MyFrame.Navigate(typeof(Calculator));
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //if (CalculatorListBoxItem.IsSelected) { ResultTextBlock.Text = "Calculator"; }
+            //else if (FavoritesListBoxItem.IsSelected) { ResultTextBlock.Text = "Favorites"; }
+        }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
